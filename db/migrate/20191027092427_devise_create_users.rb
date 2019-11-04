243331b5d4554ab86002fb8db0e00559eee26904
Kosuke-I <ki.program0915@gmail.com>
2019-11-04 11:18:12 +0900

    deploy6

--- db/migrate/20191027092427_devise_create_users.rb
+++ db/migrate/20191027092427_devise_create_users.rb
@@ -37,9 +37,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :models, :email,                unique: true
-    add_index :models, :reset_password_token, unique: true
-    # add_index :models, :confirmation_token,   unique: true
-    # add_index :models, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
